e067ac4945df44370e20e481e91aa3d3efc906be
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-14 00:00:19 +0600

    learning variables today

---
+++ js-day02/variables.js
@@ -0,0 +1,41 @@
+// const firstName = "Tanvir Hasan";
+// let age = 24;
+// let city = "Mymensingh";
+// let isStudent = true;
+
+// console.log(firstName);
+// console.log(age);
+// console.log(city);
+// console.log(isStudent);
+
+// age = age + 1;
+// console.log(age);
+
+// let x = 100;
+// x = x + 500;
+// x = x + 20;
+// console.log(x);
+
+// const y = 50;
+// console.log(y);
+// y = 70;
+
+// const b = 80;
+
+// b = 89;
+
+// console.log(b);
+
+let x = 29;
+const y = 39;
+
+x = x + y;
+
+console.log(x);
+console.log(y);
+
+// let city;
+// console.log(city);
+let city = null;
+
+console.log(city);

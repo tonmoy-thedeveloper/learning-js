7108cac50488cc1bf1bb1de38e779026b6231ea8
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-14 17:57:29 +0600

    Learning comparison

---
+++ js-day02/comparison.js
@@ -0,0 +1,17 @@
+// Greater Than >
+console.log(10 > 20);
+console.log(20 > 12);
+
+// Less tha <
+console.log(12 < 13);
+console.log(140 < 130);
+
+// Greater than or equal >=
+console.log(20 >= 15);
+console.log(230 >= 230);
+console.log(230 >= 250);
+
+// Less than or equal <=
+console.log(20 <= 21);
+console.log(4 <= 4);
+console.log(5 <= 3);
---
+++ js-day02/practice-comparison.js
@@ -0,0 +1,32 @@
+// Task 1               Prediction before execution
+console.log(25 > 20); // true
+
+// Task 2
+console.log(15 <= 10); // false
+
+// Task 3
+console.log("50" == 50); // true
+
+// Task 4
+console.log("10" === 10); // False
+
+// Task 5
+console.log("100" !== 100); // true
+
+// level 2
+
+// Task 1
+let age = 22;
+let minAge = 18;
+
+console.log(age >= minAge); // true
+
+// Task 2
+let password = "12345";
+let enteredPassword = "12345";
+console.log(password === enteredPassword); // true
+
+// Task 3
+let price = 400;
+let budget = 300;
+console.log(budget >= price); // false

0ca085643dd6de743da3ddf81a4b7c3e4f3e8fe1
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-14 15:46:28 +0600

    Learning operators

---
+++ js-day02/operator.js
@@ -0,0 +1,32 @@
+// I set my price
+// let coffeePrice = 250;
+// let pastryPrice = 300;
+
+// // How much money I have
+// let myWallet = 1000;
+
+// // Total cost of my breakfast
+// let totalCost = coffeePrice + pastryPrice;
+
+// // I'm calculating my change
+// let myChange = myWallet - totalCost;
+
+// console.log("Total cost :", totalCost);
+// console.log("My change : ", myChange);
+
+// Task 02
+// let pizzasOrdered = 5;
+// let slicesPerPizza = 6;
+
+// let totalSlices = pizzasOrdered * slicesPerPizza;
+// console.log(totalSlices);
+// let numOfFriends = 6;
+// let slicePerPerson = totalSlices / numOfFriends;
+// console.log(slicePerPerson);
+
+// Task 03
+let totalDonuts = 14;
+let coworkers = 4;
+
+let donutsForMe = totalDonuts % coworkers;
+console.log(donutsForMe);

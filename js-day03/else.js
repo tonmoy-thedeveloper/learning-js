ba32575c9faa806933ea78f42d29b434471824f5
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-16 20:39:39 +0600

    Learning if-else

---
+++ js-day03/else.js
@@ -0,0 +1,21 @@
+let isStudent = false;
+if (isStudent === true) {
+  console.log("Yes. He's a student");
+} else {
+  console.log("No. He is not a student");
+}
+
+const mangoPrice = 1900;
+const budget = 2000;
+if (mangoPrice <= budget) {
+  console.log("I will by some KG's of mango");
+} else {
+  console.log("I can't afford to buy mango");
+}
+
+let rikshawFare = 80;
+if (rikshawFare <= 50) {
+  console.log("I will go in a rikshaw");
+} else {
+  console.log("I will go by walk");
+}
---
+++ js-day03/if.js
@@ -0,0 +1,11 @@
+if (true) {
+  console.log("Condition is true and code run inside it");
+}
+
+if (50 >= 40) {
+  console.log("Bigger");
+}
+
+if (49 <= 53) {
+  console.log("It is lower");
+}

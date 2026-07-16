8a7170288b232402ec125fb4cd43c97534e7f194
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-16 16:26:55 +0600

    Learning JS conditionals

---
+++ js-day03/conditional.js
@@ -0,0 +1,31 @@
+let age = 10;
+
+if (age >= 18) {
+  console.log("You can vote....");
+} else {
+  console.log("You cannot vote..");
+}
+
+let num = 13;
+if (num > 10) {
+  console.log("Big boy");
+}
+
+let personAge = 17;
+if (personAge >= 18) {
+  console.log("You can drive.");
+} else {
+  console.log("Mother fucker are you crazy.");
+}
+
+let marks = 12;
+
+if (marks >= 90) {
+  console.log("You get A+ for this marks " + marks);
+} else if (marks >= 80) {
+  console.log("You get A for this marks " + marks);
+} else {
+  console.log(
+    "Such a failure dumbAss you get B for this mark " + marks + " Ewwwwwww",
+  );
+}

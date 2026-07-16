ce1bbea513a93c2d3a6ec0a3ff48bee283408cb9
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-16 21:41:03 +0600

    Learned about multiple conditons like && and ||

---
+++ js-day03/multi.js
@@ -0,0 +1,30 @@
+let age = 18;
+let hasId = true;
+let hasPremium = false;
+
+if (age >= 18 && hasId === true && hasPremium === true) {
+  console.log("You can easily Join to our club");
+} else {
+  console.log("Who the fuck you think you are?");
+}
+
+const donation = null;
+const hasReference = false;
+const haveQuota = false;
+
+if (donation >= 5000000 || hasReference === true || haveQuota === true) {
+  console.log("You are a perfect candidate for Government Job");
+} else {
+  console.log("I will call you after sometimes.");
+  console.log("Employer: HaHa! fucking hope full youth.");
+}
+
+const monthlyIncome = 30000;
+const hasPoliticalRef = true;
+const supporterOfRulingParty = true;
+
+if ((hasPoliticalRef && supporterOfRulingParty) || monthlyIncome >= 300000) {
+  console.log("You can easily live in this country...");
+} else {
+  console.log("You should die asap");
+}

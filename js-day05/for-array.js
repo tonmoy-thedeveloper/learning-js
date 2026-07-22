464e010c71725fd8bd950c84ed7d13189802fd82
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-22 07:20:19 +0600

    for loop with array

---
+++ js-day05/for-array.js
@@ -0,0 +1,24 @@
+// Task 01
+let colors = ["Red", "Green", "Blue"];
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+// Task 02
+let numbers = [5, 10, 15, 20];
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// Task 03
+let animals = ["Cat", "Dog", "Tiger", "Lion"];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log("Animal:", animals[i]);
+}
+
+// Task 04
+let cities = ["Dhaka", "Tokyo", "London"];
+for (let i = 0; i < cities.length; i++) {
+  console.log("City", i + 1, ":", cities[i]);
+}

3c1a34d8bb2b4e5a6593186aa39b72bc7f4a34a8
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-22 06:34:25 +0600

    Index of method

---
+++ js-day05/indexof.js
@@ -0,0 +1,4 @@
+let friends = ["John", "Adolf", "Hitler", "ImRan"];
+
+console.log(friends.indexOf("Hitler"));
+console.log(friends.indexOf("Adolf"));
---
+++ js-day05/methods.js
@@ -0,0 +1,6 @@
+const numbers = [20, 30];
+numbers.push(40);
+numbers.unshift(10);
+numbers.pop();
+numbers.push(50);
+console.log(numbers);

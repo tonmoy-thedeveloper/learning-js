db6ac45d2fd0e5f2038c57b3bde2a2a85143d655
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-30 04:05:35 +0600

    Learned how to reverse a array without revers method

---
+++ js-day05/reverse.js
@@ -0,0 +1,38 @@
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let reversedArray = [];
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//   reversedArray.push(numbers[i]);
+//   //   console.log(numbers[i]);
+// }
+// console.log(reversedArray);
+// Task 1
+let colors = ["Red", "Green", "Blue"];
+let reversedColor = [];
+for (let i = colors.length - 1; i >= 0; i--) {
+  reversedColor.push(colors[i]);
+}
+console.log(reversedColor);
+
+// Task 2
+let numbers = [5, 10, 15, 20];
+let reversedNumber = [];
+for (let i = numbers.length - 1; i >= 0; i--) {
+  reversedNumber.push(numbers[i]);
+}
+console.log(reversedNumber);
+// Task 3
+
+let names = ["Tom", "Alex", "Sara", "John"];
+let reversedNameOrder = [];
+for (let i = names.length - 1; i >= 0; i--) {
+  reversedNameOrder.push(names[i]);
+}
+console.log(reversedNameOrder);
+
+let letters = ["A", "B", "C"];
+for (let i = letters.length - 1; i >= 0; i--) {
+  console.log(i, letters[i]);
+}
+
+let fruits = ["Apple", "Kiwi", "Jack fruit"];
+console.log(fruits[fruits.length]);

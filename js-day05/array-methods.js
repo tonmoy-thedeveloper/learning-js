800e596452e855a3f0b88f0d45db4db01e069d7a
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-30 19:22:10 +0600

    Learned concat, reverse, at and other array methods

--- js-day05/array-methods.js
+++ js-day05/array-methods.js
@@ -73,19 +73,100 @@
 // console.log(love.split("i"));
 
 // Task 1
-let colors = ["Red", "Green", "Blue"];
-let joinColors = colors.join(" | ");
-console.log(joinColors);
+// let colors = ["Red", "Green", "Blue"];
+// let joinColors = colors.join(" | ");
+// console.log(joinColors);
+
+// // Task 2
+// let text = "HTML,CSS,JavaScript,React";
+// console.log(text.split(","));
+
+// // Task 3
+// let sentence = "I want to become a Web Developer";
+// console.log(sentence.split(" "));
+
+// // Task 4
+// let word = "Programming";
+// console.log(word.split(""));
+
+//  Learning concat()
+// let frontend = ["HTML", "CSS"];
+// let backend = ["Node.js", "Express"];
+
+// let fullstack = frontend.concat(backend);
+// console.log(fullstack);
+
+// let a = [1, 2];
+// let b = [3, 4];
+// let c = [5, 6];
+// let combineArray = a.concat(b, c);
+// console.log(combineArray);
+
+// // Add individual value
+// let numbers = [1, 2];
+// let addValue = numbers.concat(3, 5);
+// console.log(addValue);
+
+// // Mix values
+
+// let result = numbers.concat([33, 55, 55], addValue, 33);
+// console.log(result);
+
+// Task 1
+let fruits1 = ["Apple", "Banana"];
+let fruits2 = ["Orange", "Mango"];
+
+let combineFruits = fruits1.concat(fruits2);
+console.log(combineFruits);
 
 // Task 2
-let text = "HTML,CSS,JavaScript,React";
-console.log(text.split(","));
+let frontend = ["HTML", "CSS"];
+let addValue = frontend.concat("JavaScript");
+console.log(addValue);
+console.log(frontend);
 
 // Task 3
-let sentence = "I want to become a Web Developer";
-console.log(sentence.split(" "));
-// Task 4
-let word = "Programming";
-console.log(word.split(""));
+let a = [1, 2];
+let b = [3, 4];
+let c = [5, 6];
 
-console.log(colors.join(100));
+// let result = a.concat(b, c);
+// console.log(result);
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+let result = arr1.concat(arr2);
+
+arr1.push(100);
+
+console.log(result);
+console.log(arr1);
+
+// Learning reverse() and at()
+
+let arrayOfNum1 = [12, 3, 44, 566, 22, 23];
+console.log(arrayOfNum1.reverse());
+
+// Task 1
+let colors = ["Red", "Green", "Blue"];
+let reverseArray = colors.reverse();
+
+// Tsdk 2
+let numbers = [10, 20, 30, 40];
+console.log(numbers.at(-1));
+
+// Task 3
+let fruits = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits.at(0));
+console.log(fruits.at(-1));
+console.log(fruits.at(-2));
+
+let arr = [1, 2, 3];
+
+let reversed = arr.reverse();
+
+reversed.push(100);
+
+console.log(arr);
+console.log(reversed);
